501682f8dd087e5f59b9c801af49ca8441480da9
KORJin <ing013@naver.com>
2022-09-06 21:22:10 +0900

    Ver01

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,7 @@ module com.example.demo {
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
-    
+
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
 
